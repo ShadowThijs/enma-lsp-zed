8fd8f7ce8eb469d544e4884e0bfe57c6dc2f8538
ShadowThijs <logtenbergthijs@yahoo.com>
2026-05-25 23:19:24 +0200

    fix: remove accidentally committed tree-sitter-enma/target/

--- tree-sitter-enma/target/debug/deps/find_msvc_tools-faefae7bd6b8243c.d
+++
@@ -1,9 +0,0 @@
-/mnt/extra/github/sinister.codes/enma-lsp-zed/tree-sitter-enma/target/debug/deps/find_msvc_tools-faefae7bd6b8243c.d: /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs
-
-/mnt/extra/github/sinister.codes/enma-lsp-zed/tree-sitter-enma/target/debug/deps/libfind_msvc_tools-faefae7bd6b8243c.rlib: /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs
-
-/mnt/extra/github/sinister.codes/enma-lsp-zed/tree-sitter-enma/target/debug/deps/libfind_msvc_tools-faefae7bd6b8243c.rmeta: /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs
-
-/home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/lib.rs:
-/home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/find_tools.rs:
-/home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/find-msvc-tools-0.1.9/src/tool.rs:
--- tree-sitter-enma/target/debug/deps/tree_sitter_language-592d9ab12427b1b2.d
+++
@@ -1,7 +0,0 @@
-/mnt/extra/github/sinister.codes/enma-lsp-zed/tree-sitter-enma/target/debug/deps/tree_sitter_language-592d9ab12427b1b2.d: /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-language-0.1.7/src/language.rs
-
-/mnt/extra/github/sinister.codes/enma-lsp-zed/tree-sitter-enma/target/debug/deps/libtree_sitter_language-592d9ab12427b1b2.rlib: /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-language-0.1.7/src/language.rs
-
-/mnt/extra/github/sinister.codes/enma-lsp-zed/tree-sitter-enma/target/debug/deps/libtree_sitter_language-592d9ab12427b1b2.rmeta: /home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-language-0.1.7/src/language.rs
-
-/home/thijs/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tree-sitter-language-0.1.7/src/language.rs:
