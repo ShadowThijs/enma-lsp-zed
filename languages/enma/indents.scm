d54eabdb97ecaa0e4bf3e5a02a6924ba148ff987
ShadowThijs <logtenbergthijs@yahoo.com>
2026-05-26 23:13:02 +0200

    fix: fix autoclosing and auto indenting

--- languages/enma/indents.scm
+++ languages/enma/indents.scm
@@ -1,19 +1,4 @@
 ; Enma indentation queries - structured grammar
 
-; Indent after opening braces
-(block "{" @indent)
-(struct_body "{" @indent)
-(class_body "{" @indent)
-(enum_declaration "{" @indent)
-(namespace_definition "{" @indent)
-(switch_statement "{" @indent)
-(initializer_list "{" @indent)
-(designated_initializer "{" @indent)
-
-; Indent after control flow without braces
-(if_statement consequence: (_) @indent)
-(if_statement alternative: (_) @indent)
-(while_statement body: (_) @indent)
-(for_statement body: (_) @indent)
-(for_each_statement body: (_) @indent)
-(do_statement body: (_) @indent)
+(_ "{" "}" @end) @indent
+(_ "(" ")" @end) @indent
